e3fd000e16596be23f945bd9bfce25b47522a1c3
Mohammed Faheem <faheemmint978@gmail.com>
2024-01-26 18:09:07 +0530

    chart added

--- expensetracker/src/app/chart/chart.component.ts
+++ expensetracker/src/app/chart/chart.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.showChartData()
+  }
+
+  showChartData(){
+    new Chart("myChart", {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+            tension: .2
+          },
+        ],
+    
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+          
+        },
+        
+      },
+    });
+  }
 
 }
